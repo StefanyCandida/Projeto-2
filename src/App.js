1531359547fb4065d97046b51248a916ab3fc8fb
StefanyCandida <stefany.candida@gmail.com>
2022-02-07 12:12:49 -0300

    inserindo projeto2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+ class ClassComponent extends Component{
+render (){
+return(
+<div>
+<h1>Projeto Class Componet</h1>
+<p> Foguete não dá ré</p>
+</div>
+);
+}
+}
+export default ClassComponent
